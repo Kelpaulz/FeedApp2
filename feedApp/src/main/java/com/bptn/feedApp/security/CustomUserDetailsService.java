534673b1ef9f9fd8d981417249cf18598d7cdb14
Kelpaulz <asianyapaul@gmail.com>
2023-03-20 12:45:58 -0600

    Feed App - Security Configuration customUserDetailsService

---
+++ feedApp/src/main/java/com/bptn/feedApp/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.bptn.feedApp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import com.bptn.feedApp.jpa.User;
+import com.bptn.feedApp.repository.UserRepository;
+
+@Service("UserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		Optional<User> opt = this.userRepository.findByUsername(username);
+
+		if (opt.isEmpty()) {
+			throw new UsernameNotFoundException("Username not found: " + username);
+		}
+
+		return new CustomUserDetails(opt.get());
+	}
+
+}
