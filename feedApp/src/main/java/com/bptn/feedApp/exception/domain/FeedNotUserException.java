745d55d083280cb18dae70cc9c77085031d8b1aa
Kelpaulz <asianyapaul@gmail.com>
2023-03-20 11:06:35 -0600

    Feed App Exception Handling

---
+++ feedApp/src/main/java/com/bptn/feedApp/exception/domain/FeedNotUserException.java
@@ -0,0 +1,13 @@
+package com.bptn.feedApp.exception.domain;
+
+public class FeedNotUserException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public FeedNotUserException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
---
+++ feedApp/src/main/java/com/bptn/feedApp/exception/domain/LikeExistException.java
@@ -0,0 +1,12 @@
+package com.bptn.feedApp.exception.domain;
+
+public class LikeExistException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public LikeExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
