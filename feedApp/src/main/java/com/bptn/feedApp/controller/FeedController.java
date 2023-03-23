975944416669babc6ad321cd2d18e2a16b6e1f74
Kelpaulz <asianyapaul@gmail.com>
2023-03-23 09:23:48 -0600

    Get request for feed controller

--- feedApp/src/main/java/com/bptn/feedApp/controller/FeedController.java
+++ feedApp/src/main/java/com/bptn/feedApp/controller/FeedController.java
@@ -1,4 +1,5 @@
 package com.bptn.feedApp.controller;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bptn.feedApp.domain.PageResponse;
 import com.bptn.feedApp.jpa.Feed;
 import com.bptn.feedApp.service.FeedService;
 
@@ -20,21 +22,28 @@ public class FeedController {
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	FeedService feedService;
-	
+
 	@PostMapping
 	public Feed createFeed(@RequestBody Feed feed) {
-				
+
 		logger.debug("Creating Feed");
-				
+
 		return this.feedService.createFeed(feed);
 	}
-	
+
 	@GetMapping("/{feedId}")
 	public Feed getFeed(@PathVariable int feedId) {
-			
+
 		logger.debug("Getting Feed, feedId: {}", feedId);
-			
-		return this.feedService.getFeedById(feedId);	
+
+		return this.feedService.getFeedById(feedId);
 	}
 
+	@GetMapping("/user/{pageNum}/{pageSize}")
+	public PageResponse<Feed> getUserFeeds(@PathVariable int pageNum, @PathVariable int pageSize) {
+
+		logger.debug("Getting User Feeds List, pageNum: {}, pageSize: {}", pageNum, pageSize);
+
+		return this.feedService.getUserFeeds(pageNum, pageSize);
+	}
 }
